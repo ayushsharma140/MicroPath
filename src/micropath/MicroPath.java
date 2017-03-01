63a6502098ac7f2555e13349b3f1e0af91f8c324
ArinjayaKhare1 <arinjayakhare1@gmail.com>
2017-03-02 01:13:51 +0530

    first commit

---
+++ src/micropath/MicroPath.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micropath;
+
+/**
+ *
+ * @author ArinjayaKhare1
+ */
+import micropath.microPathStarts;
+public class MicroPath {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        microPathStarts micro=new microPathStarts();
+        micro.setVisible(true);
+        
+        // TODO code application logic here
+    }
+    
+}
